d10a1f8587d9e7c9fb961e991240ed09a3b14c5c
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2024-02-14 14:44:21 +0500

    add arg parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, ops::Deref};
+// use core::slice::Iter;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    
+    let file_path = get_env_param(&args, "-f");
+    let query = get_env_param(&args,"-q");
+
+    println!("file_path: {file_path:?}, query: {query:?}")
+
+}
+
+
+
+fn get_env_param(args: &Vec<String>, key: &str) -> Option<String> {
+    let mut is_last_eq_key = false;
+
+    for item in args {
+        if is_last_eq_key {
+            return Some(item.to_string());
+        }
+        if item == key {
+            is_last_eq_key = true;
+        }
+    };
+    
+    None
 }
